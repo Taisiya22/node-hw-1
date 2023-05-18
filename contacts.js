ff71e8f9d456a49ea0154c3e7ac592f7b2d5f22c
Taisiya Tomashevska <taya22011985@gmail.com>
2023-05-18 09:59:44 +0300

    FORMATED

--- contacts.js
+++ contacts.js
@@ -1,48 +1,46 @@
 const fs = require("fs/promises");
 const path = require("path");
-const {nanoid} = require('nanoid')
-
+const { nanoid } = require("nanoid");
 
 const contactsPath = path.join(__dirname, "db", "contacts.json");
-console.log(contactsPath)
+console.log(contactsPath);
 
-// TODO: задокументувати кожну функцію
 async function listContacts() {
-    const data = await fs.readFile(contactsPath);
-    return JSON.parse(data);
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
 }
 
 async function getContactById(contactId) {
-    const contacts = await listContacts();
-    const results = contacts.find(item => item.id === contactId);
-    return results || null;
+  const contacts = await listContacts();
+  const results = contacts.find((item) => item.id === contactId);
+  return results || null;
 }
 
 async function removeContact(contactId) {
-    const contacts = await listContacts();
-    const index = contacts.findIndex(item => item.id === contactId)
-    if (index === -1) { 
-        return null;
-    }
-    const [results] = contacts.splice(index, 1);
-     await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-    return results;
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [results] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return results;
 }
 
 async function addContact(data) {
-    const contacts = await listContacts();
-    const newContact = {
-        id: nanoid(),
-        ...data,
-    }
-    contacts.push(newContact);
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-    return newContact;
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    ...data,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
 }
 
 module.exports = {
-    listContacts,
-    getContactById,
-    addContact,
-    removeContact,
-}
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+};
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-
-const {program} = require("commander");
-const contacts = require('./contacts');
+const { program } = require("commander");
+const contacts = require("./contacts");
 
 program
   .option("-a, --action <type>", "choose action")
@@ -10,33 +9,25 @@ program
   .option("-p, --phone <type>", "user phone");
 
 program.parse();
-
 const argv = program.opts();
 
-
-// TODO: рефакторить
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-       const allContacts = await contacts.listContacts();
-             console.table(allContacts);
-          break;
-      case "get":
-          const oneContact = await contacts.getContactById(id);
-          console.log(oneContact);
-          break;
-      case "add":
-          const newContact = await contacts.addContact({ name, email, phone });
-          console.log(newContact);
-      case "remove":
-          const deleteContact = await contacts.removeContact(id);
-          console.log(deleteContact)
+      const allContacts = await contacts.listContacts();
+      console.table(allContacts);
+      break;
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      console.log(oneContact);
+      break;
+    case "add":
+      const newContact = await contacts.addContact({ name, email, phone });
+      console.log(newContact);
+    case "remove":
+      const deleteContact = await contacts.removeContact(id);
+      console.log(deleteContact);
   }
 }
 
 invokeAction(argv);
-
-// invokeAction({ action: "list" });
-// invokeAction({action:"get", id: "AeHIrLTr6JkxGE6SN-0Rw"})
-// invokeAction({action:"add", name:"Taisiya", email:"taya@gmail.com", phone: "0981234567"})
-// invokeAction({action:"remove", id: "AeHIrLTr6JkxGE6SN-0Rw"})
